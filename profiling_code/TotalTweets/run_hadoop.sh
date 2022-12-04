121dddd2de19335746a8783f8fc361ee28d23a07
DanielJang99 <daniel.jang.0621@gmail.com>
2022-12-03 21:55:17 -0500

    removed scripts

--- profiling_code/TotalTweets/run_hadoop.sh
+++
@@ -1,1 +0,0 @@
-hadoop jar CountTotalTweets.jar CountTotalTweets /user/hsj276/tweets/2018/12 /user/hsj276/profiling_data/total_tweets/201812 &
--- profiling_code/WeatherTweets/run_hadoop.sh
+++
@@ -1,1 +0,0 @@
-hadoop jar CountWeatherTweets.jar CountWeatherTweets /user/hsj276/tweets/2018/11 /user/hsj276/profiling_data/weather_tweets/201811 &
